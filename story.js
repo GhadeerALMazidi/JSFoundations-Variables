91422ab9df9759d93c45858dca6c9cb6a22f3fb7
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2020-10-18 22:37:52 +0300

    story.js

---
+++ story.js
@@ -0,0 +1,4 @@
+let username = "Mshary" 
+let number = 25
+let hobby = "Meditation" 
+console.log (`Mr.${username}, in 33 years you will be ${number + 33}. by then you will have decades of ${hobby} practice`)
